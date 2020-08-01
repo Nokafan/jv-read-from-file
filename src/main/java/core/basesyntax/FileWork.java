04967bbb2354841c9410388616260b6fd6dec014
Nokafan <volodymy.stepura@gmail.com>
2020-08-01 15:36:52 +0300

    dosn't work camel case

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <p>Дано файл, потрібно прочитати його вміст і вибрати всі слова що починаються на `w`.
  * Результат повернути у вигляді відсортованого масиву (за замовчуванням). Всі слова повинні
@@ -9,6 +18,37 @@ package core.basesyntax;
  */
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        return null;
+        List<String> inputLines = new ArrayList<>();
+        String[] resultArray = new String[]{};
+
+        try {
+            inputLines = Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            return resultArray;
+        }
+
+        List<String> resultWords = new ArrayList<>();
+
+        for (String string : inputLines) {
+            string = string.toLowerCase();
+//            System.out.println(string);
+            string = string.replaceAll("[^a-z] | (\\.)", " ");
+//            string = string.replaceAll(" ", ".");
+            System.out.println(string);
+
+//            List<String> tempList = Arrays.asList(string.split(" "));
+
+            String[] arrayWords = string.split("\\s");
+
+            for (String oneWord : arrayWords) {
+                System.out.println(oneWord);
+                if (oneWord.charAt(0) == 'w') {
+                    resultWords.add(oneWord);
+                }
+            }
+
+        }
+        Collections.sort(resultWords);
+        return resultWords.toArray(resultArray);
     }
 }
